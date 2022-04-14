548cacff6f484d2912d0ebdee418d54739f179ec
Civitasv <hscivitasv@gmail.com>
2022-04-14 14:37:30 +0800

    start refcell

---
+++ more_about_cargo_and_cratesio/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn test_doc() {}
+
+pub mod test_module {}
--- more_about_cargo_and_cratesio/src/main.rs
+++ more_about_cargo_and_cratesio/src/main.rs
@@ -1,3 +1,35 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities...
+//! This comment is to document the item that contains
+//! this comment, in this case, it's main.rs
+//!
+
+use more_about_cargo_and_cratesio::test_doc;
+use more_about_cargo_and_cratesio::test_module;
+
 fn main() {
     println!("Hello, world!");
+}
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+///
+/// # Panics
+///
+/// # Errors
+///
+/// # Safety
+///
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
 }
