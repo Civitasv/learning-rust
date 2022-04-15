f4fd15961b441507e770c137d1ecbb07addbb5dd
Civitasv <hscivitasv@gmail.com>
2022-04-15 12:02:33 +0800

    macros start

---
+++ advanced_features/src/main.rs
@@ -0,0 +1,74 @@
+fn main() {
+    let person = Human;
+    person.fly();
+    Pilot::fly(&person);
+}
+
+trait MYIterator {
+    type Item;
+    fn next(&mut self) -> Option<Self::Item>;
+}
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+use std::fmt;
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+// impl OutlinePrint for Human {} // it's not valid
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice<F>(f: F, arg: i32) -> i32
+where
+    F: Fn(i32) -> i32,
+{
+    f(arg) + f(arg)
+}
+
+fn do_twice2(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+fn returns_function() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(add_one)
+}
+
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
